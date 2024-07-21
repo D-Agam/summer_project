1674ed60146d2d1afb130f2289133ebbe64cbf5d
D-Agam <agamdawra01@gmail.com>
2024-07-21 19:43:01 +0530

    Pages frontend

--- Frontend/src/main_sample1.jsx
+++ Frontend/src/main_sample1.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import App1 from './App1.js'
 import './index.css'
-
+// import './index1.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <App1 />
   </React.StrictMode>,
 )
